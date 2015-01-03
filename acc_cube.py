d1732cd6d8010160113f599f3b450a40cb602a02
Jeremy Mill <jeremymill@gmail.com>
2015-01-03 17:41:52 -0500

    finally fixed all the tabs

--- acc_cube.py
+++ acc_cube.py
@@ -2,11 +2,11 @@
 This is a cube imitating the position of an accelerometer.
 
 The cube drawing is based off of code by Leonel Machava <leonelmachava@gmail.com>
-	http://codeNtronix.com
-	
+    http://codeNtronix.com
+    
 The accelerometer data/interface is based off of code by Bitify and can be found at:
-	http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
-	Author: Andrew Birkett
+    http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
+    Author: Andrew Birkett
 """
 
 import sys, math, pygame, smbus
@@ -91,24 +91,24 @@ class Simulation:
         #get and set initial values
         #accelerometer
         x = self.acc.get_x_scaled()
-	y = self.acc.get_y_scaled()
-	z = self.acc.get_z_scaled()
-	#rotation accelerometer data
-	self.last_x_angle = self.acc.get_x_rotation(x,y,z)
-	self.last_y_angle = self.acc.get_y_rotation(x,y,z)
-	#gyro
-	self.gyro_offset_x = self.acc.get_GX_scaled()
-	self.gyro_offset_y = self.acc.get_GY_scaled()
-	#gyro totals
-	self.gyro_total_x = self.last_x_angle - self.gyro_offset_x
-	self.gyro_total_y = self.last_y_angle - self.gyro_offset_y
-	
-	#time now
-	self.time_one = time.time()
-	
-	#set K values
-	self.K = 0.98
-	self.K1 = 1-self.K
+        y = self.acc.get_y_scaled()
+        z = self.acc.get_z_scaled()
+        #rotation accelerometer data
+        self.last_x_angle = self.acc.get_x_rotation(x,y,z)
+        self.last_y_angle = self.acc.get_y_rotation(x,y,z)
+        #gyro
+        self.gyro_offset_x = self.acc.get_GX_scaled()
+        self.gyro_offset_y = self.acc.get_GY_scaled()
+        #gyro totals
+        self.gyro_total_x = self.last_x_angle - self.gyro_offset_x
+        self.gyro_total_y = self.last_y_angle - self.gyro_offset_y
+        
+        #time now
+        self.time_one = time.time()
+        
+        #set K values
+        self.K = 0.98
+        self.K1 = 1-self.K
         
     def run(self):
         """ Main Loop """
@@ -130,73 +130,57 @@ class Simulation:
             x = self.acc.get_x_scaled()
             y = self.acc.get_y_scaled()
             z = self.acc.get_z_scaled()
-	    #scaled gyro values
-	    gx = self.acc.get_GX_scaled()
-	    gy = self.acc.get_GY_scaled()
-	    gz = self.acc.get_GZ_scaled()
-			
-	    #modify gx/y/z with the offsets
-	    gx -= self.gyro_offset_x
-	    gy -= self.gyro_offset_y
-			
-	    #set the gyro delta values
-	    time_two = time.time()
-	    time_diff = time_two - self.time_one
-	    self.time_one = time_two
-			
-	    gx_delta = gx * time_diff
-	    gy_delta = gy * time_diff
-			
-	    #new gyro totals
-	    self.gyro_total_x += gx_delta
-	    self.gyro_total_y += gy_delta
-			
-	    #get the accelerometer rotation values
-	    self.x_angle = self.acc.get_x_rotation(x,y,z)
-	    self.y_angle = self.acc.get_y_rotation(x,y,z)
-			
-	    #combine them to filter out noise
-	    self.last_x_angle = self.K * (self.last_x_angle + gx_delta ) + (self.K1 * self.x_angle)
-	    self.last_y_angle = self.K * (self.last_y_angle + gy_delta ) + (self.K1 * self.y_angle)
-			
-			
+            #scaled gyro values
+            gx = self.acc.get_GX_scaled()
+            gy = self.acc.get_GY_scaled()
+            gz = self.acc.get_GZ_scaled()
+            
+            #modify gx/y/z with the offsets
+            gx -= self.gyro_offset_x
+            gy -= self.gyro_offset_y
+            
+            #set the gyro delta values
+            time_two = time.time()
+            time_diff = time_two - self.time_one
+            self.time_one = time_two
+            
+            gx_delta = gx * time_diff
+            gy_delta = gy * time_diff
+            
+            #new gyro totals
+            self.gyro_total_x += gx_delta
+            self.gyro_total_y += gy_delta
+            
+            #get the accelerometer rotation values
+            self.x_angle = self.acc.get_x_rotation(x,y,z)
+            self.y_angle = self.acc.get_y_rotation(x,y,z)
+            
+            #combine them to filter out noise
+            self.last_x_angle = self.K * (self.last_x_angle + gx_delta ) + (self.K1 * self.x_angle)
+            self.last_y_angle = self.K * (self.last_y_angle + gy_delta ) + (self.K1 * self.y_angle)
+            
+            
             
             for v in self.vertices:
-				
-				#i'm going to try and move this out of the for loop
-				
-				#get values from the accelerometer
-				#x = self.acc.get_x_scaled()
-				#y = self.acc.get_y_scaled()
-				#z = self.acc.get_z_scaled()
-				
-				#get the rotation values
-				#self.x_angle = self.acc.get_x_rotation(x,y,z)
-				#self.z_angle = self.acc.get_y_rotation(x,y,z)
-				
-				#this next value will be the z when I figure it out
-				#self.z_angle = self.acc.get_z_rotation(x,y,z)
-				
-				
-				'''here, rotateX is forward and backwards
-				rotateY is around a vertical axis with positive numbers meaning CCW(left) rotation
-				rotateZ is rotating around an axis pointing straight at you with positive numbers rotating CCW (left hand)'''
-				
-				'''this means that the roll is along the x axis, and the pitch is along the z axis. This is opposite from the
-				accelerometer, which is confusing as hell, but it's easier to modify here than rewrite'''
-				#change x and z angles and leave y alone because we have no yaw data
-				
-				#!!!HERE IS WHERE WE CHANGE THE Y AND THE Z!!!
-				
-				r = v.rotateX(self.last_x_angle).rotateY(self.z_angle).rotateZ(self.last_y_angle)
-				
-				# Transform the point from 3D to 2D
-				p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
-				
-				# Put the point in the list of transformed vertices
-				t.append(p)
-				
-			# Calculate the average Z values of each face.
+                '''here, rotateX is forward and backwards
+                rotateY is around a vertical axis with positive numbers meaning CCW(left) rotation
+                rotateZ is rotating around an axis pointing straight at you with positive numbers rotating CCW (left hand)'''
+        
+                '''this means that the roll is along the x axis, and the pitch is along the z axis. This is opposite from the
+                accelerometer, which is confusing as hell, but it's easier to modify here than rewrite'''
+                #change x and z angles and leave y alone because we have no yaw data
+                
+                #!!!HERE IS WHERE WE CHANGE THE Y AND THE Z!!!
+                
+                r = v.rotateX(self.last_x_angle).rotateY(self.z_angle).rotateZ(self.last_y_angle)
+                
+                # Transform the point from 3D to 2D
+                p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
+                
+                # Put the point in the list of transformed vertices
+                t.append(p)
+                
+        # Calculate the average Z values of each face.
             avg_z = []
             i = 0
             for f in self.faces:
@@ -215,92 +199,87 @@ class Simulation:
                              (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
                 pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
 
-                
-            #self.angle += 1
-            
-            
-            
+
             pygame.display.flip()
             
 class Accelerometer:
-	def __init__(self):
-			
-		# Power management registers
-		self.power_mgmt_1 = 0x6b
-		self.power_mgmt_2 = 0x6c
-		
-		self.bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
-		self.address = 0x68       # This is the address value read via the i2cdetect command
-		
-		# Now wake the 6050 up as it starts in sleep mode
-		self.bus.write_byte_data(self.address, self.power_mgmt_1, 0)
-		
-	def read_byte(self,adr):
-		return self.bus.read_byte_data(self.address, adr)
+    def __init__(self):
+        # Power management registers
+        self.power_mgmt_1 = 0x6b
+        self.power_mgmt_2 = 0x6c
+        
+        self.bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
+        self.address = 0x68       # This is the address value read via the i2cdetect command
+        
+        # Now wake the 6050 up as it starts in sleep mode
+        self.bus.write_byte_data(self.address, self.power_mgmt_1, 0)
+        
+    def read_byte(self,adr):
+        return self.bus.read_byte_data(self.address, adr)
 
-	def read_word(self,adr):
-		self.high = self.bus.read_byte_data(self.address, adr)
-		self.low = self.bus.read_byte_data(self.address, adr+1)
-		val = (self.high << 8) + self.low
-		return val
+    def read_word(self,adr):
+        self.high = self.bus.read_byte_data(self.address, adr)
+        self.low = self.bus.read_byte_data(self.address, adr+1)
+        val = (self.high << 8) + self.low
+        return val
 
-	def read_word_2c(self,adr):
-		val = self.read_word(adr)
-		if (val >= 0x8000):
-			return -((65535 - val) + 1)
-		else:
-			return val
+    def read_word_2c(self,adr):
+        val = self.read_word(adr)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
 
-	def dist(self,a,b):
-		return math.sqrt((a*a)+(b*b))
+    def dist(self,a,b):
+        return math.sqrt((a*a)+(b*b))
 
-	#the math here is that roll = atan(y/(sqrt(x^2 + z^2)))
-	#we use atan2 because it automaticall determines the sign for us (the quadrant)
-	def get_y_rotation(self,x,y,z):
-		radians = math.atan2(x, self.dist(y,z))
-		return -math.degrees(radians)
+    #the math here is that roll = atan(y/(sqrt(x^2 + z^2)))
+    #we use atan2 because it automaticall determines the sign for us (the quadrant)
+    def get_y_rotation(self,x,y,z):
+        radians = math.atan2(x, self.dist(y,z))
+        return -math.degrees(radians)
 
-	#the math here is that the pitch is = atan(x/(sqrt(y^2 + z^2)))
-	#we use atan2 because it automaticall determines the sign for us (the quadrant)
-	def get_x_rotation(self,x,y,z):
-		radians = math.atan2(y, self.dist(x,z))
-		return math.degrees(radians)
-	
-	#as a note here, the 'scaled' values are the values in G's
-	def get_x_scaled(self):
-		self.accel_xout = self.read_word_2c(0x3b)
-		self.accel_xout_scaled = self.accel_xout / 16384.0
-		return self.accel_xout_scaled
-	
-	#as a note here, the 'scaled' values are the values in G's	
-	def get_y_scaled(self):
-		self.accel_yout = self.read_word_2c(0x3d)
-		self.accel_yout_scaled = self.accel_yout / 16384.0
-		return self.accel_yout_scaled
-	
-	#as a note here, the 'scaled' values are the values in G's	
-	def get_z_scaled(self):
-		self.accel_zout = self.read_word_2c(0x3f)
-		self.accel_zout_scaled = self.accel_zout / 16384.0
-		return self.accel_zout_scaled
-		
-	def get_GX_scaled(self):
-		GX = self.read_word_2c(0x43)
-		GX_scaled = GX/131
-		return GX_scaled
-		
-	def get_GY_scaled(self):
-		GY = self.read_word_2c(0x45)
-		GY_scaled = GY/131
-		return GY_scaled
-		
-	def get_GZ_scaled(self):
-		GZ = self.read_word_2c(0x47)
-		GZ_scaled = GZ/131
-		return GZ_scaled
-		
-	def testclass(self):
-		return "test"
+    #the math here is that the pitch is = atan(x/(sqrt(y^2 + z^2)))
+    #we use atan2 because it automaticall determines the sign for us (the quadrant)
+    def get_x_rotation(self,x,y,z):
+        radians = math.atan2(y, self.dist(x,z))
+        return math.degrees(radians)
+    
+    #as a note here, the 'scaled' values are the values in G's
+    def get_x_scaled(self):
+        self.accel_xout = self.read_word_2c(0x3b)
+        self.accel_xout_scaled = self.accel_xout / 16384.0
+        return self.accel_xout_scaled
+    
+    #as a note here, the 'scaled' values are the values in G's  
+    def get_y_scaled(self):
+        self.accel_yout = self.read_word_2c(0x3d)
+        self.accel_yout_scaled = self.accel_yout / 16384.0
+        return self.accel_yout_scaled
+    
+    #as a note here, the 'scaled' values are the values in G's  
+    def get_z_scaled(self):
+        self.accel_zout = self.read_word_2c(0x3f)
+        self.accel_zout_scaled = self.accel_zout / 16384.0
+        return self.accel_zout_scaled
+        
+    def get_GX_scaled(self):
+        GX = self.read_word_2c(0x43)
+        GX_scaled = GX/131
+        return GX_scaled
+        
+    def get_GY_scaled(self):
+        GY = self.read_word_2c(0x45)
+        GY_scaled = GY/131
+        return GY_scaled
+        
+    def get_GZ_scaled(self):
+        GZ = self.read_word_2c(0x47)
+        GZ_scaled = GZ/131
+        return GZ_scaled
+        
+    def testclass(self):
+        return "test"
 
 
 if __name__ == "__main__":
