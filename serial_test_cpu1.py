49181ac4aaf60bcea3ebb47859710aaae01207b0
Jeremy Mill <jeremymill@gmail.com>
2015-01-17 21:49:33 -0500

    adding xbee testing

---
+++ serial_test_cpu1.py
@@ -0,0 +1,12 @@
+import serial
+import io
+import py_acc
+import time as Timer
+
+port = "/dev/ttyUSB0"
+baud = 9600
+time_out = 5
+
+xbee = serial.Serial(port,baudrate=baud,timeout=time_out)
+
+xbee.write(unicode("ping\n"))
